d1048c412c3a1a82f45beb2ebb09a1b2a7d254ad
Reethiika <113743168+Reethiika@users.noreply.github.com>
2022-11-19 23:17:53 +0530

    Update final code.py

--- Final Deliverables/Final Code/final code.py
+++ Final Deliverables/Final Code/final code.py
@@ -23,6 +23,28 @@ test_df.drop(test_df.columns[[26, 27]], axis=1, inplace=True)
 test_df.columns = ['id', 'cycle', 'setting1', 'setting2', 'setting3', 's1', 's2', 's3',
                      's4', 's5', 's6', 's7', 's8', 's9', 's10', 's11', 's12', 's13', 's14',
                      's15', 's16', 's17', 's18', 's19', 's20', 's21']
+
+
+# read ground truth data
+truth_df = pd.read_csv('PM_truth.txt', sep=" ", header=None)
+truth_df.drop(truth_df.columns[[1]], axis=1, inplace=True)
+
+train_df = train_df.sort_values(['id','cycle'])
+train_df.head()
+
+rul = pd.DataFrame(train_df.groupby('id')['cycle'].max()).reset_index()
+rul.columns = ['id', 'max']
+train_df = train_df.merge(rul, on=['id'], how='left')
+train_df['RUL'] = train_df['max'] - train_df['cycle']
+train_df.drop('max', axis=1, inplace=True)
+train_df.head()
+
+w1 = 30
+w0 = 15
+train_df['label1'] = np.where(train_df['RUL'] <= w1, 1, 0 )
+train_df['label2'] = train_df['label1']
+train_df.loc[train_df['RUL'] <= w0, 'label2'] = 2
+train_df.head()
                      
                      
                      
